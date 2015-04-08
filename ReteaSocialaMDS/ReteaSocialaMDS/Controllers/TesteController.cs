e5035e819aa0e6f2570cbd0a1840f59c0fc012dc
Robert Stelian Sandu <robert-stelian.sandu@my.fmi.unibuc.ro>
2015-04-08 10:40:45 +0300

    Teste Initiale Layout

---
+++ ReteaSocialaMDS/ReteaSocialaMDS/Controllers/TesteController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReteaSocialaMDS.Controllers
+{
+    public class TesteController : Controller
+    {
+        // GET: Teste
+        public ActionResult Index(String mesaj, int varsta=0)
+        {
+            ViewBag.MessagePrimit = mesaj;
+            ViewBag.Message = "Gigel este pe camp";
+            ViewBag.Varsta = varsta;
+            return View();
+        }
+
+        public ActionResult lista()
+        {
+            return PartialView();
+        }
+        
+    }
+}
