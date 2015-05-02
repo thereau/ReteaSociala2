0c885da0ee2770ea0d389a20d544bdce1eb72a92
Robert Stelian Sandu <robert-stelian.sandu@my.fmi.unibuc.ro>
2015-05-03 00:55:51 +0300

    diverse modificari

---
+++ ReteaSocialaMDS/ReteaSocialaMDS/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReteaSocialaMDS.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int ID { get; set; }
+        public string UserID { get; set; }
+       // [Required]
+
+        //public string Comment { get; set; }
+    }
+}
