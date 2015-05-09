ee4d02488ba218ad1c99503633ccbdfeb8fcb2ea
Robert Stelian Sandu <robert-stelian.sandu@my.fmi.unibuc.ro>
2015-05-09 19:01:32 +0300

    sincronizeaza tot boss, nu doar modificarile in filele existente

---
+++ ReteaSocialaMDS/ReteaSocialaMDS/Models/Post.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ReteaSocialaMDS.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+     
+
+
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser User { get; set; }
+
+        public string PostMessage { get; set; }
+
+        private DateTime? postDate;
+
+        public DateTime PostDate
+        {
+            get { return postDate ?? DateTime.Now; }
+            set { postDate = value; }
+        }
+
+        public virtual ICollection<PostComment> PostComments { get; set; }
+
+
+    }
+}
---
+++ ReteaSocialaMDS/ReteaSocialaMDS/Models/PostComment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ReteaSocialaMDS.Models
+{
+    public class PostComment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public int ParentPostId { get; set; }
+
+
+
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser User { get; set; }
+
+        [ForeignKey("ParentPostId")]
+        public virtual Post ParentPost { get; set; }
+
+        public string PostCommentMessage { get; set; }
+
+        private DateTime? commentPostDate;
+
+        public DateTime CommentPostDate
+        {
+            get { return commentPostDate ?? DateTime.Now; }
+            set { commentPostDate = value; }
+        }
+    }
+}
